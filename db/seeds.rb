958d2b9302a592a7acff6577c43b1b8450d12c27
Jessica Moura De Sousa <jessicamoura15@gmail.com>
2016-03-29 20:22:17 -0300

    Added function for saving pt-BR names

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,9 @@
 File.foreach('db/roaring-skies.txt').with_index do |line|
   att = line.split
-  Card.create(number: att[0], name_en: att[1], card_type: att[2], rarity: att[3], expansion: "Roaring Skies")
+  Card.create(number: att[0].split('/')[0], name_en: att[1], card_type: att[2], rarity: att[3], expansion: "Roaring Skies")
+end
+File.foreach('db/roaring-skies_pt.txt').with_index do |line|
+  att = line.split
+  card = Card.find_by(number: att[0], expansion: "Roaring Skies")
+  card.update(name_pt: att[1])
 end
