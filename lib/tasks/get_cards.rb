f0469bc70e3ce9262ee00e8d8a5c80fc7a64df23
Jessica Moura De Sousa <jessicamoura15@gmail.com>
2016-03-30 02:46:29 -0300

    Added method to update cards with photos from serebii.net

--- lib/tasks/get_cards.rb
+++
@@ -1,4 +0,0 @@
-require 'csv'
-CSV.foreach('db/roaring-skies.csv') do |row|
-  Card.create(number: row[0].split('/')[0], name_en: row[1], element: row[2], rarity: row[3], expansion: "Roaring Skies")
-end
--- lib/tasks/update_cards.rb
+++
@@ -1,5 +0,0 @@
-File.foreach('db/roaring-skies_pt.txt').with_index do |line|
-  row = line.split(/(\d+)/)
-  card = Card.find_by(number: row[1], expansion: "Roaring Skies")
-  card.update(name_pt: row[2].strip)
-end
