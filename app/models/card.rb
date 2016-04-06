593af77d7c9667535c623b220f7f008cd9c6b261
Jessica Moura De Sousa <jessicamoura15@gmail.com>
2016-04-05 21:49:50 -0300

    Added card name update

--- app/models/card.rb
+++ app/models/card.rb
@@ -8,6 +8,7 @@ class Card < ActiveRecord::Base
       if card_hash.fetch("number")
         card.attributes = card_hash.except("expansion")
         card.number = card.number.split('/')[0]
+        card.name_en = card.name_en.gsub(/(?<=[a-z])(?=[A-Z])/, ' ')
         card.save!
       end
     end
